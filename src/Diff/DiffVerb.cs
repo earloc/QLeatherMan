171f4acab41d4badbdd308e601378bb116688260
earloc <earloc@live.de>
2020-02-09 01:26:15 +0100

    simplifiy usage

--- src/Diff/DiffVerb.cs
+++ src/Diff/DiffVerb.cs
@@ -6,10 +6,10 @@ namespace QLeatherMan.Diff
     [Verb("diff", HelpText = "performs a difference-analysis of two GraphQL-schemas")]
     public class DiffVerb
     {
-        [Option('l', Required = true, HelpText = "Uri of the left hand side for the diff-comparison")]
+        [Value(0, Required = true, HelpText = "Uri of the left hand side for the diff-comparison")]
         public Uri? LeftUri { get; set; }
 
-        [Option('r', Required = true, HelpText = "Uri of the right hand side for the diff-comparison")]
+        [Value(1, Required = true, HelpText = "Uri of the right hand side for the diff-comparison")]
         public Uri? RightUri { get; set; }
 
         [Option('o', Required = false, HelpText = "Path to file for generaten comparison report in Markdown-Format")]
